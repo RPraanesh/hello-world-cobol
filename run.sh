96b059b7406ef93d166ee634a26e53129e577726
RPraanesh <45656825+RPraanesh@users.noreply.github.com>
2018-12-14 12:02:14 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ clear
 printf "\n\n\n"
 printf "\nThe source code can be found in hello-world.cbl\n\n"
 
-printf "Run \"cobc -free -x -o hello-world hello-world.cbl\" to compile.\n\n----------------------------------\n"
+printf "Run \"cobc -x hello-world.cbl" to compile.\n\n----------------------------------\n"
